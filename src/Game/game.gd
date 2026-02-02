b6335031be2cdd5b21570191ec9a996ed93b7b41
Seantx <sean.cab@gmail.com>
2026-02-02 14:05:22 -0600

    v3
    This version is completely human controlled (there is a slight issue with pressing stand immediately ends the round instead of letting the computer take a turn) but we will fix this in the next version by giving the computer an AI

--- src/Game/game.gd
+++ src/Game/game.gd
@@ -1,6 +1,11 @@
 extends Node2D
 class_name Game
 
+@export var PlayerMoney:Label
+@export var DealerMoney:Label
+@export var PlayerScore:Label
+@export var DealerScore:Label
+
 @onready var deck: Deck
 @onready var nodedeck: NodeDeck
 @onready var hand: Hand
@@ -11,25 +16,47 @@ class_name Game
 @onready var draw_no =0
 @onready var players = 2
 var player_turn: bool = true
+var player_bust = false
+var dealer_bust = false
+var push = false
 var player_cards_dealt = 0
 var dealer_cards_dealt = 0
 var cards_dealt = player_cards_dealt + dealer_cards_dealt
 var player_total = 0
 var dealer_total = 0
+var round_over: bool = false
 var hand_size = 2
+
+var player_money = 5
+var dealer_money = 5
+var pot = 1
 
 func _ready() -> void:
 	print('leggo')
 	deck = Deck.new()
-	nodedeck=NodeDeck.new()
 	print(deck.cards)
 	starter_deal()
+	player_turn = true
+	DealerMoney.text = str("$",dealer_money)
+	PlayerMoney.text = str("$",player_money) 
 	#deal_hand()
 	#add_child(card_shell)
 	
-func _physics_process(delta: float) -> void:	
+func _physics_process(delta: float) -> void:
+	PlayerScore.text = str(player_total)
+	DealerScore.text = str(dealer_total)
+	if round_over:
+		if Input.is_action_just_pressed('restart'):
+			if %DealerBust.visible:
+				%DealerBust.visible = false
+			if %Bust.visible:
+				%Bust.visible = false
+			new_round()
+	if Input.is_action_just_pressed('stand') and player_turn:
+			player_turn = false
+			print('stand')
 	if player_turn:
-		if Input.is_action_just_pressed('hit'):
+		if Input.is_action_just_pressed('hit') and not player_bust:
 			var card_def = deck.cards.pop_front()
 			var new_card = card_shell.instantiate()
 			new_card.face_value = card_def[1]
@@ -39,62 +66,54 @@ func _physics_process(delta: float) -> void:
 			
 			new_card.global_position = %PlayerHand.global_position + Vector2(player_cards_dealt * 100,0)
 			
-			player_total += new_card.face_value
+			player_total += clamp(new_card.face_value,0,10)
 			print(player_total)
-			add_child(new_card)
+			$Cards.add_child(new_card)
 			player_cards_dealt += 1
-			
 			
 			if player_total > 21:
 				%Bust.visible = true
-				if Input.is_action_just_pressed("restart"):
-					get_tree().reload_current_scene()
-			
-			if Input.is_action_just_pressed('stand'):
-				player_turn = false
-			
-		
-		 
-	if Input.is_action_just_pressed("draw"):
-		var new_card = nodedeck.cards[draw_no]
-		var face_label = Label.new()
-		var suit_label = Label.new()
-		var card_face = ColorRect.new()
-		face_label.text = str(new_card.face_value)
-		suit_label.text = str(new_card.suit_value)
-		suit_label.global_position = face_label.global_position + Vector2(0,100)
-		card_face.color = Color.WHITE
-		new_card.add_child(card_face)
-		new_card.add_child(face_label)
-		new_card.add_child(suit_label)
-		add_child(new_card)
-		print(new_card)
-		draw_no += 1
-
-func starter_deal() -> void:
-	for bettors in players:
-		for card in hand_size:
+				player_bust = true
+				round_end()
+				
+				
+	if not player_turn:
+		if Input.is_action_just_pressed('hit'):
 			var card_def = deck.cards.pop_front()
 			var new_card = card_shell.instantiate()
-			
 			new_card.face_value = card_def[1]
 			new_card.get_node('Control/FaceLabel').text = str(card_def[0])
 			new_card.suit_value = card_def[0]
 			new_card.get_node('Control/SuitLabel').text = str(card_def[1])
 			
-			if player_turn:
-				new_card.global_position = %PlayerHand.global_position + Vector2(player_cards_dealt * 100,0)
-				player_turn = false
-				player_cards_dealt += 1
-				player_total += new_card.face_value
-			else:
-				new_card.global_position = %DealerHand.global_position + Vector2(dealer_cards_dealt * 100,0)
-				player_turn = true
-				dealer_cards_dealt += 1
-				dealer_total += new_card.face_value
+			new_card.global_position = %DealerHand.global_position + Vector2(dealer_cards_dealt * 100,0)
 			
-			print(player_total)
-			add_child(new_card)
+			dealer_total += clamp(new_card.face_value,0,10)
+			print('dealer turn ', dealer_total)
+			$Cards.add_child(new_card)
+			dealer_cards_dealt += 1
+			if dealer_total > 21:
+				%DealerBust.visible = true
+				dealer_bust = true
+				round_end()
+		if Input.is_action_just_pressed('stand'):
+			if player_total > dealer_total:
+				dealer_bust = true
+			if dealer_total > player_total:
+				player_bust = true
+			if dealer_total == player_total:
+				push = true
+			round_end()
+
+func starter_deal() -> void:
+	for bettors in players:
+		for card in hand_size:
+			deal_card()
+			if player_turn:
+				player_turn = false
+			else:
+				player_turn = true
+
 	
 func deal_card() -> void:
 	var card_def = deck.cards.pop_front()
@@ -107,14 +126,42 @@ func deal_card() -> void:
 	
 	if player_turn:
 		new_card.global_position = %PlayerHand.global_position + Vector2(player_cards_dealt * 100,0)
-		player_turn = false
 		player_cards_dealt += 1
-		player_total += new_card.face_value
+		player_total += clamp(new_card.face_value,0,10)
 	else:
 		new_card.global_position = %DealerHand.global_position + Vector2(dealer_cards_dealt * 100,0)
-		player_turn = true
 		dealer_cards_dealt += 1
-		dealer_total += new_card.face_value
+		dealer_total += clamp(new_card.face_value,0,10)
 	
 	print(player_total)
-	add_child(new_card)
+	$Cards.add_child(new_card)
+	#PlayerScore.text = str(player_total)
+	#DealerScore.text = str(dealer_total)
+
+func round_end() -> void:
+	print('round end')
+	if not push:
+		if player_bust:
+			player_money = player_money - pot 
+			dealer_money = dealer_money + pot
+		if dealer_bust:
+			player_money = player_money + pot
+			dealer_money = dealer_money - pot
+	DealerMoney.text = str("$",dealer_money)
+	PlayerMoney.text = str("$",player_money)
+	round_over = true
+
+func new_round() -> void:
+	player_bust = false
+	dealer_bust = false
+	push = false
+	player_cards_dealt = 0
+	dealer_cards_dealt = 0
+	cards_dealt = player_cards_dealt + dealer_cards_dealt
+	player_total = 0
+	dealer_total = 0
+	for n in $Cards.get_children(): n.queue_free()
+	deck = Deck.new()
+	starter_deal()
+	player_turn = true
+	
